ea4a2a2893cf966d19cf4ba6907742283cb5210c
Pepe Valdivia <jvaldivia@softweb.pe>
2020-11-02 17:16:04 -0500

    listar especilidades

---
+++ src/main/java/pe/softweb/config/Database.java
@@ -0,0 +1,36 @@
+package pe.softweb.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.javalite.activejdbc.DB;
+import org.javalite.activejdbc.DBException;
+import java.util.Properties;
+
+public class Database {
+  private DB db;
+
+  public Database() {
+    this.db = new DB();
+  }
+
+  public DB getDb() {
+    return this.db;
+  }
+
+  public void open() throws DBException, IOException {
+    InputStream input = new FileInputStream("src/main/resources/application.properties");
+    Properties properties = new Properties();
+    properties.load(input);
+    // Database instance
+    String driver = properties.getProperty("db.driver");
+    String url = properties.getProperty("db.url");
+    String user = properties.getProperty("db.user");
+    String password = properties.getProperty("db.password");
+	  this.db.open(driver, url, user, password);
+  }
+
+  public void close(){
+    this.db.close();
+  }
+}
---
+++ src/main/java/pe/softweb/controller/SpecialismController.java
@@ -0,0 +1,45 @@
+package pe.softweb.controller;
+
+import org.json.JSONArray;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pe.softweb.config.Database;
+import pe.softweb.model.Specialism;
+
+@RestController
+@RequestMapping(
+  value = "/specialism"
+)
+public class SpecialismController extends ApplicationController
+{
+  @RequestMapping(
+    value = {"/list", "/listar"}, 
+    method = RequestMethod.GET,
+    produces={"text/html; charset=utf-8"}  
+  )
+  public ResponseEntity<String> list() 
+  {
+    String response = "";
+    HttpStatus status = HttpStatus.OK;
+    Database db = new Database();
+    try {
+      db.open();
+      response = Specialism.findAll().toJson(true).toString();
+    }catch (Exception e) {
+      e.printStackTrace();
+      JSONArray error = new JSONArray();
+      error.put("Se ha producido un error en listar las especialidades");
+      error.put(e.toString());
+      response = error.toString();
+      status = HttpStatus.INTERNAL_SERVER_ERROR;
+    } finally {
+      if(db.getDb().hasConnection()){
+        db.close();
+      }
+    }
+    return new ResponseEntity<>(response, status);
+  }
+}
